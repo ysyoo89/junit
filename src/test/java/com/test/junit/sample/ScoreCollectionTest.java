95147d275c877376175d2e73f8436755a05f75d6
ysyoo89 <ysyoo89@kakao.com>
2023-05-10 17:37:03 +0900

    JUnit Right-BICEP 조건

--- src/test/java/com/test/junit/sample/ScoreCollectionTest.java
+++ src/test/java/com/test/junit/sample/ScoreCollectionTest.java
@@ -1,11 +1,15 @@
 package com.test.junit.sample;
 
-import org.junit.jupiter.api.Test;
+import com.test.junit.scratch.AssertTest;
+import org.junit.Test;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreCollectionTest {
+
+    private ScoreCollection collection;
 
     @Test
     void testArithmeticMean() {
@@ -23,5 +27,32 @@ class ScoreCollectionTest {
 
         // 단언
         assertThat(actualResult).isEqualTo(6);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void throwsExceptionWhenAddingNull() {
+        collection.add(null);
+    }
+
+    @Test
+    public void answersZeroWhenNoElementsAdded() {
+        assertThat(collection.arithmeticMean()).isEqualTo(0);
+    }
+
+    @Test
+    public void dealsWithIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertThat(collection.arithmeticMean()).isEqualTo(1073741824);
+
+    }
+
+    @Test
+    public void doesNotProperlyHandleIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertTrue(collection.arithmeticMean() < 0);
     }
 }
